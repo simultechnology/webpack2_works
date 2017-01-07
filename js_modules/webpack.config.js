111790033a662e0638195b2663f7fb6cc11dfcbe
t_ishikawa <tissi0708@gmail.com>
2017-01-08 00:51:56 +0900

    ビルドプロセスにcssとimageを追加

--- js_modules/webpack.config.js
+++ js_modules/webpack.config.js
@@ -1,19 +1,42 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'), // absolute path
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: 'build/'
   },
   module: {
     rules: [
       {
         use: 'babel-loader',
         test: /\.js$/
+      },
+      {
+        //use: ['style-loader', 'css-loader'],
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader'
+        }),
+        test: /\.css$/
+      },
+      {
+        test: /\.(jpe?g|gif|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 }
+          },
+          //'url-loader',
+          'image-webpack-loader'
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 };
 
 module.exports = config;
